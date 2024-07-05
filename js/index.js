0b87194f1a76e8c3d251cfdca870628a8baf4755
salmaaa9 <smsmss279201@yahoo.com>
2024-07-05 22:12:34 +0300

    first section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+console.log($('.sideNav').width());
+
+$('.close').on('click',function(){
+    $('.sideNav').css({left : -$('.sideNav').width()})
+})
 $('#menu').on('click',function(){
     this.color = 'red'
     console.log('clicked');
+    $('.sideNav').css({left : 0})
 })
