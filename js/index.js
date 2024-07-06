acb7b301d40ab64acd72c6b9e3f7852d568ab1f7
salmaaa9 <smsmss279201@yahoo.com>
2024-07-06 16:20:21 +0300

    second section

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
-console.log($('.sideNav').width());
-
+// sideNav display
 $('.close').on('click',function(){
     $('.sideNav').css({left : -$('.sideNav').width()})
 })
-$('#menu').on('click',function(){
-    this.color = 'red'
-    console.log('clicked');
+$('.options').on('click',function(){
+    $('.menu').removeClass('d-none')
     $('.sideNav').css({left : 0})
 })
+
+// sliderDown
+$('#sliderDown h3').click(function() {
+
+    const parent = $(this).closest('.second');
+    parent.find('p.inner').not($(this).next()).slideUp(500);
+    
+    const section = $(this).next();
+    console.log(section);
+  
+    // Toggle the paragraph within the section
+    $(this).next().slideToggle(500);
+
+  });
+  
